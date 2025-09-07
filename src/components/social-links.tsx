c57f3be21169afec1caa8caed6097a9f02d3ff37
Priyanku Gogoi <114563767+ppriyankuu@users.noreply.github.com>
2025-09-07 07:51:55 +0530

    Update latest insta link

--- src/components/social-links.tsx
+++ src/components/social-links.tsx
@@ -3,7 +3,7 @@ import { FaLinkedin, FaInstagram, FaGithub } from 'react-icons/fa';
 const SocialLinks = () => {
   const links = [
     { name: 'LinkedIn', icon: FaLinkedin, url: 'https://www.linkedin.com/in/priyanku-gogoi', color: 'text-blue-500' },
-    { name: 'Instagram', icon: FaInstagram, url: 'https://instagram.com/ppriyankuu_', color: 'text-pink-500' },
+    { name: 'Instagram', icon: FaInstagram, url: 'https://instagram.com/_dempho', color: 'text-pink-500' },
     { name: 'GitHub', icon: FaGithub, url: 'https://github.com/ppriyankuu', color: 'text-gray-200' },
   ];
 
